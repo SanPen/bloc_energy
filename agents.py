3310219d5f3fd0bc43a175d6453b21a1409075d5
sanpen <santiago.penate.vera@gmail.com>
2018-10-06 11:41:43 +0200

    Added the transaction and transactions class Added the agents file with a dummy agent

---
+++ agents.py
@@ -0,0 +1,8 @@
+from GridCal.Engine.All import *
+
+
+
+class Agent:
+
+    def __init__(self):
+        pass
--- transactions.py
+++ transactions.py
@@ -1,3 +1,5 @@
+import collections
+from GridCal.Engine.All import *
 
 
 class Transaction:
@@ -17,3 +19,36 @@ class Transaction:
         self.tipology = tipology
 
         self.hash = hash
+
+
+class Transactions(collections.MutableSequence):
+    """
+    Class to store he transactions
+    """
+
+    def __init__(self, l=list()):
+        if type(l) is not list:
+            raise ValueError()
+
+        self.transactions = l
+
+    def __len__(self):
+        return len(self.transactions)
+
+    def __delitem__(self, index):
+        self.transactions.__delitem__(index - 1)
+
+    def insert(self, index, value):
+        self.transactions.insert(index - 1, value)
+
+    def __setitem__(self, index, value):
+        self.transactions.__setitem__(index - 1, value)
+
+    def __getitem__(self, index):
+        return self.transactions.__getitem__(index - 1)
+
+    def check_all(self):
+        pass
+
+    def compute_last_hash(self):
+        pass
