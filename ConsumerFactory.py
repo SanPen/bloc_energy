5b056240c2ee503813f7136be2b3dd370f6820dd
rcontreras <rcontreras@indra.es>
2018-10-07 12:47:46 +0200

    bid module finsished

--- ConsumerFactory.py
+++ ConsumerFactory.py
@@ -22,7 +22,9 @@ class ConsumerFactory:
         self.scalefactor = 0
 
         if fpath is None:
-            self.fpath = "D:\\BlockChain\\RESIDENTIAL_LOAD_DATA_E_PLUS_OUTPUT\\BASE\\USA_MA_Boston-Logan.Intl.AP.725090_TMY3_BASE.csv"
+            self.fpath = "consumer1"
+        else:
+            self.fpath = fpath
 
         if reparto is None:
             self.reparto = {'Electricity:Facility [kW](Hourly)': (100, 0, 0),
@@ -37,6 +39,7 @@ class ConsumerFactory:
                             'Misc:InteriorEquipment:Electricity [kW](Hourly)': (0, 100, 0)}
 
         self.bidprices = [maximum_price, maximum_price*0.5, maximum_price*0.2]
+
 
    def importcsv(self):
         """
@@ -71,16 +74,19 @@ class ConsumerFactory:
         flexible = [0]*24
         hours = self.mean.index.values
         for key, value in self.reparto.items():
-            mandatory_data += self.mean[key].values*self.profile[key][0]/100
-            optional += self.mean[key].values * self.profile[key][1]/100
-            flexible += self.mean[key].values * self.profile[key][2]/100
-            self.offers = {'mandatory': mandatory_data, 'optional' : optional, 'flexible': flexible}
+            if key in self.mean.columns.values:
+                mandatory_data += self.mean[key].values*self.profile[key][0]/100
+                optional += self.mean[key].values * self.profile[key][1]/100
+                flexible += self.mean[key].values * self.profile[key][2]/100
+                self.offers = {'mandatory': mandatory_data, 'optional' : optional, 'flexible': flexible}
             pass
 
    def getbid(self, nhours ):
        # Mandatorty
        # Optional
        # Flexible
+        self.importcsv()
+        self.agregate()
         mandatory = list()
         optional = list()
         flexible = list()
@@ -96,16 +102,47 @@ class ConsumerFactory:
                 optional.append((i, self.offers['optional'][i], self.bidprices[1]))
                 flexible.append((i, self.offers['flexible'][i], self.bidprices[2]))
             i+=1
-        return {'mandatory': mandatory, 'optional': optional, 'flexible': flexible}
+        return [mandatory, optional,  flexible]
+
+class GeneratorFactory:
+    def __init__(self, fpath = 'solargen', max_price = 180, scale_factor = 1):
+        self.fpath = fpath
+        self.importcsv()
+        self.maxprice = max_price
+        self.scaleFactor = scale_factor
+        pass
+
+
+    def importcsv(self):
+        self.profile = pd.read_csv(self.fpath)
+        new_origin = datetime.datetime.now().replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+        new_origin_ts = new_origin.timestamp()
+        new_dates = [datetime.datetime.fromtimestamp(new_origin_ts + n * 3600) for n in range(self.profile.shape[0])]
+        self.profile['Date/Time'][:] = new_dates
+        self.profile.set_index(keys=['Date/Time'], drop=True, inplace=True)
+        self.mean = self.profile.groupby(self.profile.index.hour).mean()
+        self.mean = self.profile.groupby(self.profile.index.hour).mean()
+
+    def getbid(self):
+        current_hour = datetime.datetime.now().hour
+
+        return (current_hour, self.mean.values[current_hour][0]*self.scaleFactor, self.maxprice)
+
+    def getmax(self):
+        pass
 
 
 if __name__ == "__main__":
 
-    filetest = "D:\\BlockChain\\RESIDENTIAL_LOAD_DATA_E_PLUS_OUTPUT\\BASE\\USA_MA_Boston-Logan.Intl.AP.725090_TMY3_BASE.csv"
-    a = ConsumerFactory(fpath=filetest, scale_factor=1)
-    a.importcsv()
-    a.agregate()
-    print(a.getbid(5))
+    #filetest = "D:\\BlockChain\\RESIDENTIAL_LOAD_DATA_E_PLUS_OUTPUT\\BASE\\USA_MA_Boston-Logan.Intl.AP.725090_TMY3_BASE.csv"
+    #a = ConsumerFactory(fpath=filetest, scale_factor=1)
+    #a.importcsv()
+    #a.agregate()
+    #print(a.getbid(5))
+
+    gen = GeneratorFactory('solargen.csv')
+    gen.getmax()
+
 
 
 
--- test1.py
+++ test1.py
@@ -2,27 +2,35 @@ from actors import Consumer, Generator, ActorsGroup
 from market import Market
 from bidding import BidType, ActorBid
 from grid import MultiCircuit, transaction_checking
+from ConsumerFactory import ConsumerFactory, GeneratorFactory
 
 if __name__ == '__main__':
 
     consumer_1 = Consumer('Consumer1')
-    consumer_1.bids = [ActorBid(BidType.Compulsory, 10, 200),
-                       ActorBid(BidType.Manageable, 30, 25),
-                       ActorBid(BidType.Expendable, 50, 5)]
+    consumer1bids = ConsumerFactory(1,fpath = 'consumer1.csv', maximum_price=200).getbid(1)
+    consumer_1.bids = [ActorBid(BidType.Compulsory, consumer1bids[0][0][1], consumer1bids[0][0][2]),
+                       ActorBid(BidType.Manageable,  consumer1bids[1][0][1], consumer1bids[1][0][2]),
+                       ActorBid(BidType.Expendable,  consumer1bids[2][0][1], consumer1bids[2][0][2])]
 
     consumer_2 = Consumer('Consumer2')
-    consumer_2.bids = [ActorBid(BidType.Compulsory, 50, 200),
-                       ActorBid(BidType.Manageable, 30, 25),
-                       ActorBid(BidType.Expendable, 50, 5)]
+    consumer2bids = ConsumerFactory(1, fpath='consumer2.csv', maximum_price=195).getbid(1)
+    consumer_2.bids = [ActorBid(BidType.Compulsory, consumer2bids[0][0][1], consumer2bids[0][0][2]),
+                       ActorBid(BidType.Manageable,  consumer2bids[1][0][1], consumer2bids[1][0][2]),
+                       ActorBid(BidType.Expendable,  consumer2bids[2][0][1], consumer2bids[2][0][2])]
 
     generator_1 = Generator('Gen1')
-    generator_1.bids = [ActorBid(BidType.Compulsory, energy_mw_=10, price_=100)]
+    gen1bid = GeneratorFactory('solargen.csv', scale_factor=50, max_price=100).getbid()
+    generator_1.bids = [ActorBid(BidType.Compulsory, gen1bid[1],gen1bid[2])]
 
     generator_2 = Generator('Gen2')
-    generator_2.bids = [ActorBid(BidType.Compulsory, energy_mw_=20, price_=20)]
+    #generator_2.bids = [ActorBid(BidType.Compulsory, energy_mw_=20, price_=20)]
+    gen2bid = GeneratorFactory('solargen.csv', scale_factor=10, max_price=20).getbid()
+    generator_2.bids = [ActorBid(BidType.Compulsory, gen2bid[1], gen2bid[2])]
 
     generator_3 = Generator('Gen3')
-    generator_3.bids = [ActorBid(BidType.Compulsory, energy_mw_=10000, price_=100)]
+    #generator_3.bids = [ActorBid(BidType.Compulsory, energy_mw_=10000, price_=100)]
+    gen1bid = GeneratorFactory('solargen.csv', scale_factor=300, max_price=103).getbid()
+    generator_1.bids = [ActorBid(BidType.Compulsory, gen1bid[1], gen1bid[2])]
 
     group = ActorsGroup()
     group.add_consumer(consumer_1)
